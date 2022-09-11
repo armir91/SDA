5460e0a97a468e308f6f18a2a67df07e1f91d95c
Armir <armir.keta@gmail.com>
2022-09-11 15:28:34 +0200

    fina commit of the day

---
+++ AdvancedFeaturesEx1/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace AdvancedFeaturesEx1;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Student student1 = new Student();
+        student1.Greet();
+
+        Student student2 = new Student();
+        student2.SetAge(25);
+        student2.Greet();
+        student2.ShowAge();
+
+        Professor profesori = new Professor();
+        profesori.SetAge(50);
+        profesori.Greet();
+        profesori.Explain();
+
+    }
+
+    public class Person
+    {
+       public int age;
+
+        public void Greet()
+        {
+            Console.WriteLine("Hello!");
+        }
+
+        public void SetAge(int age)
+        {
+            this.age = age;
+        }
+    }
+
+    public class Student : Person
+    {
+
+        public void Study()
+        {
+            Console.WriteLine("I'm studying");
+        }
+
+        public void ShowAge()
+        {
+            Console.WriteLine($"My age is: {age}");
+        }
+    }
+
+    public class Professor : Person
+    {
+        public void Explain()
+        {
+            Console.WriteLine("I'm explaining!");
+        }
+    }
+}
---
+++ AdvancedFeaturesSubclasses/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace AdvancedFeaturesSubclasses;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Trumpet t1 = new Trumpet("trumpet", true, false);
+    }
+
+    public class Instrument
+    {
+        string type;
+        bool clean;
+
+        public Instrument(string type, bool clean)
+        {
+            this.type = type;
+            this.clean = clean;
+        }
+    }
+
+    public class Trumpet : Instrument
+    {
+        bool oiled;
+
+        public Trumpet(string type, bool clean, bool oiled) : base( type,  clean)
+        {
+            this.oiled = oiled;
+        }
+    }
+}
