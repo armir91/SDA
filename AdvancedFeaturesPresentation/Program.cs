a3a5d2eb53c41e1745db6f3271d991a1a5046c94
Armir <armir.keta@gmail.com>
2022-09-11 10:55:38 +0200

    initial commit

---
+++ AdvancedFeaturesPresentation/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace AdvancedFeaturesPresentation;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("We have best class ever!");
+
+        var child = new Child();
+        child.MethodOne();
+        child.Method();
+        // underscore perdoret per variabla qe nuk do perdoren
+        _ = Console.ReadLine();
+    }
+}
+
+public class Base
+{
+    public void Method()
+    {
+        Console.WriteLine("This is coming from base class");
+    }
+
+}
+
+public class Child : Base
+{
+    public void MethodOne()
+    {
+        Console.WriteLine("This is comind from child class");
+    }
+}
