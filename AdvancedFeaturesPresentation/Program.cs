6080ed0529db2966e6c6905a80ecee46cd2c3d84
Armir <armir.keta@gmail.com>
2022-09-11 12:14:12 +0200

    latest commits

--- AdvancedFeaturesPresentation/Program.cs
+++ AdvancedFeaturesPresentation/Program.cs
@@ -6,21 +6,37 @@ internal class Program
     {
         Console.WriteLine("We have best class ever!");
 
+        // This is a child oblect created from child
         var child = new Child();
         child.MethodOne();
         child.Method();
+        child.virtualMethod();
+
         // underscore perdoret per variabla qe nuk do perdoren
         _ = Console.ReadLine();
     }
 }
 
-public class Base
+public abstract class Base
 {
     public void Method()
     {
         Console.WriteLine("This is coming from base class");
     }
 
+    // this method will be hidden in child
+    public void HideMethod()
+    {
+        Console.WriteLine("Hide method from BAse");
+    }
+
+
+    public virtual void virtualMethod()
+    {
+        Console.WriteLine("This is virtual methd and is coming from base class");
+    }
+
+    /*public abstract void OverrideItem();*/
 }
 
 public class Child : Base
@@ -28,5 +44,14 @@ public class Child : Base
     public void MethodOne()
     {
         Console.WriteLine("This is comind from child class");
+    }
+
+    // This is intended to be hidden with new keyword.
+    public new void HideMethod(){}
+
+    public override void virtualMethod()
+    {
+        Console.WriteLine("No no i am a child");
+        base.virtualMethod();
     }
 }
