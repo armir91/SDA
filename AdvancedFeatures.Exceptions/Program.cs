bc31af91d2eda523761a16dc536360d8e9ca1c66
Armir <armir.keta@gmail.com>
2022-09-17 23:13:09 +0200

    latest exercises from last week

---
+++ AdvancedFeatures.Exceptions/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace AdvancedFeatures.Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a valid number: ");
+            string s = Console.ReadLine();
+            try
+            {
+                double a = int.Parse(s!);
+                Console.WriteLine("You entered a valid number!");
+
+
+                double v = Sqrt(a);
+                Console.WriteLine($"Sqrt of {a} is: {v}.");
+            }
+            catch (FormatException ex)
+            {
+
+                Console.WriteLine("Invalid integer nr!");
+                throw;
+
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("The number is too big");
+               // throw;
+
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+              //  throw;
+
+            }
+            catch
+            {
+                Console.WriteLine("Exception");
+
+                // throw;
+            }
+            finally
+            {
+                Console.WriteLine("All done!");
+            }
+            Console.WriteLine("Code executed after try-finally");
+        }
+
+        private static double Sqrt(double value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException("Sqrt for negative nr is undefined!");
+
+            }
+            return Math.Sqrt(value);
+        }
+    }
+}
---
+++ AdvancedFeatures.Interface/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace AdvancedFeatures.Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle();
+            r1.CalculateArea(100, 200);
+        }
+
+        interface IPolygon
+        {
+            // method without body
+
+            void CalculateArea(int l, int b);
+        }
+
+        class Rectangle : IPolygon
+        {
+            // implementation of methods inside interface
+
+            public void CalculateArea(int l, int b)
+            {
+                int area = l * b;
+                Console.WriteLine("Area of Rectangle: " + area);
+            }
+        }
+    }
+}
