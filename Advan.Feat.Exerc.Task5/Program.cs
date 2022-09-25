6d93c52723b1d92a007ef8b791504bbee73ad602
Armir <armir.keta@gmail.com>
2022-09-25 02:06:01 +0200

    added Task 5 - Advanced Features

---
+++ Advan.Feat.Exerc.Task5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace Advan.Feat.Exerc.Task5;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating a count variable
+        var total = 0;
+
+        // Creating an array of colors
+        int[] numbers = { 5, 5, 5, 7, 7, 7, 9, 7, 9, 9, 9, 1 };
+
+        // Counting the total number of time blue appears
+        // in the array
+        
+
+        // Displaying the count
+        for(int i = 0; i < numbers.Length; i++)
+        {
+            total = numbers.Count(c => c == numbers[i]);
+
+            Console.WriteLine($"The number {numbers[i]} occurs: {total} times!");
+
+        }
+    }
+}
